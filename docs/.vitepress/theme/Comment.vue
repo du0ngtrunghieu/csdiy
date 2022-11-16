11aa31424aeaf40ff4562af71ce0fe504094d817
mancuoj <1337381768@qq.com>
2022-11-16 16:40:02 +0800

    feat: comment dark mode

--- docs/.vitepress/theme/Comment.vue
+++ docs/.vitepress/theme/Comment.vue
@@ -1,17 +1,19 @@
 <script setup>
 import Giscus from '@giscus/vue'
 import DefaultTheme from 'vitepress/theme'
+import { useData } from 'vitepress'
 
+const { isDark } = useData()
 const { Layout } = DefaultTheme
 </script>
 
 <template>
   <Layout>
     <template #doc-after>
-      <div style="margin-top: 2rem;" >
+      <div style="margin-top: 2rem;">
         <Giscus id="comments" repo="mancuoj/discussions" repoId="R_kgDOIcaOPg" category="Announcements"
           categoryId="DIC_kwDOIcaOPs4CSlbm" mapping="url" term="畅所欲言吧 😆" strict="0" reactionsEnabled="0"
-          emitMetadata="0" inputPosition="top" theme="preferred_color_scheme" lang="zh-CN" loading="lazy" />
+          emitMetadata="0" inputPosition="top" :theme="[isDark ? 'dark' : 'light']" lang="zh-CN" loading="lazy" />
       </div>
     </template>
   </Layout>
