ba9d0d06b51178cba6f5304fdfc520e3dd545ecf
mancuoj <1337381768@qq.com>
2022-11-25 15:22:33 +0800

    feat: update

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,12 +1,8 @@
 import DefaultTheme from 'vitepress/theme'
 import Comment from './Comment.vue'
-import { createMediumZoomProvider } from './useMediumZoom'
 import './custom.css'
 
 export default {
     ...DefaultTheme,
-    Layout: Comment,
-    enhanceApp( {app, router} ) {
-        createMediumZoomProvider(app, router)
-    }
+    Layout: Comment
 }
